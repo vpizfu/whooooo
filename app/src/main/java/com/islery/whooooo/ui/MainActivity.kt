fa7c00b9693b5793cb6042ce78875c43c9c355c8
Valeryia Isakovich <lereenne@gmail.com>
2020-08-01 00:33:45 +0300

    init: android application

---
+++ app/src/main/java/com/islery/whooooo/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.islery.whooooo.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.islery.whooooo.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
