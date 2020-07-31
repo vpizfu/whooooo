a8a59a9b814f3214a6be954eac0fd53abb2e51de
trotnic <uladzislau.volchyk@gmail.com>
2020-07-31 22:39:39 +0300

    init: ✨

---
+++ whooooo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  whooooo
+//
+//  Created by Uladzislau Volchyk on 7/31/20.
+//  Copyright © 2020 Uladzislau Volchyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
